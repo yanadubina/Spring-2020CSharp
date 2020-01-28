c0e10f68bcf805c26fbac1d748c7d1e936526993
yanadubina <yana11182@gmail.com>
2020-01-28 18:34:05 -0500

    minor changes

---
+++ src/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using src;
+
+namespace YourName.CodeLou.ExerciseProject
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+          //  bool getStudentInfo = true;
+          //  while (getStudentInfo)
+          //  {
+          //     Console.WriteLine($"Enter more info? Y/N");
+          //      getStudentInfo = Console.ReadLine() == "Y";
+          //   }
+        string continueRunning = "Y";
+        while (continueRunning == "Y")
+        {
+            InputStudent();
+            Console.WriteLine("Do you want to enter another student?(Y/N)");
+            continueRunning = Console.ReadLine();
+        }
+        
+        }
+        static void InputStudent()
+        {   Console.WriteLine("Enter Student Id");
+            var studentId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter First Name");
+            var studentFirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            var studentLastName = Console.ReadLine();
+            Console.WriteLine("Enter Class Name");
+            var className = Console.ReadLine();
+            Console.WriteLine("Enter Last Class Completed");
+            var lastClass = Console.ReadLine();
+            Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
+            var lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
+            var startDate = DateTimeOffset.Parse(Console.ReadLine());
+
+            var studentRecord = new Student();
+            studentRecord.StudentId = studentId;
+            studentRecord.FirstName = studentFirstName;
+            studentRecord.LastName = studentLastName;
+            studentRecord.ClassName = className;
+            studentRecord.StartDate = startDate;
+            studentRecord.LastClassCompleted = lastClass;
+            studentRecord.LastClassCompletedOn = lastCompletedOn;
+            Console.WriteLine($"Student Id | Name |  Class "); ;
+            Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName} "); ;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Student.cs
@@ -0,0 +1,14 @@
+using System;
+namespace src
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ClassName { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public string LastClassCompleted { get; set; }
+        public DateTimeOffset LastClassCompletedOn { get; set; }
+    }
+}
