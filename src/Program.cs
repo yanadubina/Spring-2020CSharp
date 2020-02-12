b9b2e716c230e4eea474d77e767c094787b28996
yanadubina <yana11182@gmail.com>
2020-02-11 19:34:24 -0500

    Completed Challenge 2!

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using src;
 
 namespace YourName.CodeLou.ExerciseProject
@@ -14,16 +16,63 @@ namespace YourName.CodeLou.ExerciseProject
             //     Console.WriteLine($"Enter more info? Y/N");
             //      getStudentInfo = Console.ReadLine() == "Y";
             //   }
-            string continueRunning = "Y";
-            while (continueRunning == "Y")
+            var studentList = new List<Student>();
+            var studentDictionary = new Dictionary<string, Student>();
+
+            string userInput = " ";
+            while (userInput != "4")
             {
-                InputStudent();
-                Console.WriteLine("Do you want to enter another student?(Y/N)");
-                continueRunning = Console.ReadLine();
+                ShowMenu();
+                userInput = Console.ReadLine();
+                if (userInput == "1")
+                {
+                    var newStudent = InputStudent();
+                    studentList.Add(newStudent);
+                    studentDictionary.Add(newStudent.FullName, newStudent);
+                }
+                else if (userInput == "2")
+                {
+                    ShowAllStudents(studentList);
+                }
+                else if (userInput == "3")
+                {
+                    FindStudent(studentList);
+                }
             }
 
         }
-        static void InputStudent()
+        static void FindStudent(List<Student> students)
+        {
+            Console.WriteLine("Enter full student name");
+            var nameToFind = Console.ReadLine();
+            var student = students.FirstOrDefault(student => student.FullName == nameToFind);
+            PrintStudent(student);
+
+        }
+        static void PrintStudent(Student student)
+        {
+            Console.WriteLine(student.StudentId + " | " + student.FullName + " | " + student.ClassName);
+        }
+
+        static void ShowAllStudents(List<Student> students)
+        {
+            Console.WriteLine("Student ID | Name | Class");
+            foreach (var student in students)
+            {
+                PrintStudent(student);
+            }
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Enter new student");
+            Console.WriteLine("2. List all students");
+            Console.WriteLine("3. Search for student by name");
+            Console.WriteLine("4. Exit");
+
+        }
+        static Student InputStudent()
         {
             Console.WriteLine("Enter Student Id");
             var studentId = Convert.ToInt32(Console.ReadLine());
@@ -50,7 +99,9 @@ namespace YourName.CodeLou.ExerciseProject
             studentRecord.LastClassCompletedOn = lastCompletedOn;
             Console.WriteLine($"Student Id | Name |  Class "); ;
             Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName} "); ;
-            Console.ReadKey();
+            //Console.ReadKey();
+            return studentRecord;
         }
+        
     }
 }
